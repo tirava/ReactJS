b9f7edf80da5bd72c3558ad86b1ab87b7b212512
Eugene Klimov <tirava@gmail.com>
2019-12-15 22:50:45 +0300

    HW-4 fix profiles

--- 4/src/components/Profile/Profile.jsx
+++ 4/src/components/Profile/Profile.jsx
@@ -23,10 +23,19 @@ export class Profile extends Component {
 
   render() {
     const {id} = this.props.match.params;
+    const {profiles} = this.state;
+    if (id === undefined || id > +Object.keys(profiles).length) {
+      return (
+        <div>
+          <h2>Профиль чата #{id}</h2>
+          <h4>Надо создать</h4>
+        </div>
+      );
+    }
     const {name, description} = this.state.profiles[id];
     return (
       <div>
-        <h2>Профиль чата</h2>
+        <h2>Профиль чата #{id}</h2>
         <h3>{name}</h3>
         <h4>{description}</h4>
       </div>
