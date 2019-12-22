3bb0280753bce15e36abe7f355c9ec8b3385d20a
Eugene Klimov <tirava@gmail.com>
2019-12-22 22:03:27 +0300

    HW-6 chat height work as needed

--- 6/src/utils/store.js
+++ 6/src/utils/store.js
@@ -7,9 +7,6 @@ function initStore() {
   return createStore(
     initReducers,
     initialStore,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ?
-      window.__REDUX_DEVTOOLS_EXTENSION__() : () => {
-      },
   );
 }
 
