39e890d2cd769a973455c6a44cd635ded62f72cf
Eugene Klimov <tirava@gmail.com>
2019-12-23 10:40:16 +0300

    HW-6 fix bot doubling

--- 6/src/components/Messenger/Messenger.jsx
+++ 6/src/components/Messenger/Messenger.jsx
@@ -5,11 +5,6 @@ import PropTypes from 'prop-types';
 import './Messenger.sass';
 
 export class Messenger extends Component {
-  // state = {
-  //   chatId: '1',
-  //   lenMessages: 0,
-  // };
-
   static propTypes = {
     messages: PropTypes.array.isRequired,
     addNewMessage: PropTypes.func.isRequired,
@@ -17,45 +12,9 @@ export class Messenger extends Component {
     chatName: PropTypes.string,
   };
 
-  // botTimers = [];
-
-  // clearBots = () => {
-  //   this.botTimers.forEach((timer) => clearTimeout(timer));
-  //   this.botTimers = [];
-  // };
-
   sendNewMessage = (message) => {
-    // this.clearBots();
     this.props.addNewMessage(this.props.chatId, message);
   };
-
-  // componentDidUpdate() {
-  //   const {chatId, chatName, messages} = this.props;
-  //   const len = messages.length;
-  //   if (len === 0 || len === this.state.lenMessages) {
-  //     return;
-  //   }
-  //
-  //   if (this.state.chatId !== chatId) {
-  //     this.clearBots();
-  //   }
-  //   this.setState((prevState) => {
-  //     prevState.chatId = chatId;
-  //     prevState.lenMessages = len;
-  //   });
-  //
-  //   const name = messages[len - 1].author;
-  //   if (name !== botName) {
-  //     this.botTimers.push(
-  //       setTimeout(() =>
-  //         this.sendNewMessage({
-  //           author: botName,
-  //           content: `${name}, не понял, здесь чат "${chatName}"`,
-  //           date: formatDate(),
-  //         }), 1000),
-  //     );
-  //   }
-  // }
 
   render() {
     const {messages} = this.props;
--- 6/src/middlewares/botMiddleware.js
+++ 6/src/middlewares/botMiddleware.js
@@ -2,22 +2,30 @@ import {SEND_MESSAGE, sendMessage} from '../actions/messageActions';
 import {botName} from '../utils/constants';
 import {formatDate} from '../utils/utils';
 
+let botTimers = [];
+
+const clearBots = () => {
+  botTimers.forEach((timer) => clearTimeout(timer));
+  botTimers = [];
+};
+
 export default (store) => (next) => (action) => {
   if (action.type === SEND_MESSAGE) {
     const {author} = action.message;
     if (author !== botName) {
       const chatName = store.getState().chatReducer.chats[action.chatId].title;
-      const messageId = Object.keys(
-        store.getState().messageReducer.messages).length + 1;
-      setTimeout(() => store.dispatch(
+      clearBots();
+      botTimers.push(setTimeout(() => store.dispatch(
         sendMessage(
-          action.chatId, messageId,
+          action.chatId, Object.keys(
+          store.getState().messageReducer.messages).length + 1,
           {
             author: botName,
             content: `${author}, не понял, здесь чат "${chatName}"`,
             date: formatDate(),
           },
-        )), 1000);
+        )), 1000),
+      );
     }
   }
   return next(action);
